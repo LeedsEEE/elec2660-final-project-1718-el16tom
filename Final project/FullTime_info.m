ee50f3d53043dd54749f777761dc1fc953be0d7c
default <el16tom@eentpc245.leeds.ac.uk>
2017-12-07 12:18:39 +0000

    added the final bits of table view info

--- Final project/FullTime_info.m
+++ Final project/FullTime_info.m
@@ -63,17 +63,17 @@
         NRS.Company_name= @"Nationwide Retail Systems Ltd ";
         NRS.pay= @"£17,000 per year";
         NRS.hours= @"Flexible week/weekend work";
-        NRS.Description=@"requires secondary education";
+        NRS.Description=@"requires secondary education\nEmail:NRS@gmail.com";
         NRS.Type=@"IT help desk";
-        NRS.Image_1= [UIImage imageNamed: @"NRS_2.jpeg"];
+        NRS.Image_1= [UIImage imageNamed: @"NRS_2.jpeg"];//http://www.nrsltd.com/images/logo.png
         
         Job *Christeyns  = [[Job alloc]init];
         Christeyns.Company_name= @"Christeyns ";
         Christeyns.pay= @"Competitive salary";
         Christeyns.hours= @"4 full days a week";
-        Christeyns.Description=@"Requires work experience in sector";
+        Christeyns.Description=@"Requires work experience in sector\nEmail us at christeyns.employability@hotmail.co.uk";
         Christeyns.Type=@"IT support";
-        Christeyns.Image_1= [UIImage imageNamed: @"christeyns_2.png"];
+        Christeyns.Image_1= [UIImage imageNamed: @"christeyns_2.png"];//http://www.christeyns.com/sites/christeyns/themes/the_aim_theme/logo.png
         
         
         //second array
@@ -89,7 +89,7 @@
         Ecco_Pizzeria.Company_name= @"Ecco Pizzeria";
         Ecco_Pizzeria.pay= @"Competitive salary";
         Ecco_Pizzeria.hours= @"6 days a week";
-        Ecco_Pizzeria.Description=@"2 years experience required";
+        Ecco_Pizzeria.Description=@"2 years experience required\nEmail:Ecco.pizzeria@gmail.com or call:0777893470";
         Ecco_Pizzeria.Type=@"Preparing Pizza & Pasta dishes";
         Ecco_Pizzeria.Image_1= [UIImage imageNamed: @"EP_3.jpeg"];//https://pbs.twimg.com/profile_images/2645248078/95decb68ffa99ba379661e5369bfe7c6.jpeg
         
@@ -98,18 +98,18 @@
         P_L.Company_name= @"Pizza labella";
         P_L.pay= @"£18,000 a year";
         P_L.hours= @"6 evenings a week";
-        P_L.Description=@"Not cash in hand";
+        P_L.Description=@"Not cash in hand, you will be required to prepare large orders so expercience is a must.\nEmail:Pizza_labella@yahoo.com";
         P_L.Type=@"Grill chef";
-        P_L.Image_1= [UIImage imageNamed: @"labella_2.jpg"];
+        P_L.Image_1= [UIImage imageNamed: @"labella_2.jpg"];//http://slice.seriouseats.com/images/2012/01/20120118-daily-slice-la-bella-pizza-garden.jpg
         
         
         Job *EMC = [[Job alloc]init];
         EMC.Company_name= @"Everyman Cinema";
         EMC.pay= @"£15,000 a year";
         EMC.hours= @"5 shifts a week";
-        EMC.Description=@"No experience required";
+        EMC.Description=@"No experience required\nEmail:LeedsEMC@hotmail.co.uk";
         EMC.Type=@"Plating dishes";
-        EMC.Image_1= [UIImage imageNamed: @"Everyman-Cinema-Leeds_2.jpg"];
+        EMC.Image_1= [UIImage imageNamed: @"Everyman-Cinema-Leeds_2.jpg"];//https://www.thingstodowhenitsraining.co.uk/wp-content/uploads/formidable/Everyman-Cinema-Leeds.jpg
         
         
         
@@ -132,26 +132,26 @@
         Greggs.Company_name= @"Greggs";
         Greggs.pay= @"Upto £22,000 a year";
         Greggs.hours= @"Early morning shifts";
-        Greggs.Description=@"Employee benefits available if you meet the required targets.";
+        Greggs.Description=@"Employee benefits available if you meet the required targets.\nEmail-greggsleeds@gmail.com";
         Greggs.Type=@"Store manager";
-        Greggs.Image_2= [UIImage imageNamed: @"greggs_2.png"];
+        Greggs.Image_2= [UIImage imageNamed: @"greggs_2.png"];//http://www.theforumshoppingcentre.co.uk/wp-content/uploads/2017/04/Greggs.png
         
         Job *NHS = [[Job alloc]init];
         NHS.Company_name= @"NHS";
         NHS.pay= @"£20,000 a year";
         NHS.hours= @"3 days a week";
-        NHS.Description=@"Must provide 3 years of work refernece";
+        NHS.Description=@"Must provide 3 years of work refernece\n Contact info: Telephone:07552916878\nEmail:NHS_leedsemployment@hotmail.co.uk";
         NHS.Type=@"Barista";
-        NHS.Image_2= [UIImage imageNamed: @"NHS.png"];
+        NHS.Image_2= [UIImage imageNamed: @"NHS.png"];//https://i0.wp.com/www.kateshahidcoaching.co.uk/wpr2a/wp-content/uploads/NHS-Logo.png
         
         
         Job *Atlas = [[Job alloc]init];
         Atlas.Company_name= @"AtlasCorp";
         Atlas.pay= @"£18,000-£24,000 a year";
         Atlas.hours= @"Flexible hours";
-        Atlas.Description=@"Bonus package available";
+        Atlas.Description=@"Bonus package available\nOur email is AtlasCorp_contact@hotmail.com";
         Atlas.Type=@"Trainee events manager";
-        Atlas.Image_2= [UIImage imageNamed: @"AC.png"];
+        Atlas.Image_2= [UIImage imageNamed: @"AC.png"];//https://vignette1.wikia.nocookie.net/someordinarygamers/images/a/a3/Atlas_corp_logo_dark_by_imaginitivex-d85w06p.png/revision/latest?cb=20150225205759
         
         
         
@@ -167,7 +167,7 @@
         Derwent.Company_name= @"Derwent";
         Derwent.pay= @"£19,000 a year";
         Derwent.hours= @"Mon,Tues & Thurs 9-5";
-        Derwent.Description=@"Wide range of tasks";
+        Derwent.Description=@"Wide range of challenging tasks, any potential canidates will need to be prepared to take part in an assessment\nContact info:\nEmail:Derwent_contact@hotmail.com";//https://upload.wikimedia.org/wikipedia/en/0/0a/Derwent-living.jpg
         Derwent.Type=@"Help desk operator";
         Derwent.Image_2= [UIImage imageNamed: @"Derwent.png"];
         
@@ -206,7 +206,7 @@
         LCC.Company_name= @"Leeds council";
         LCC.pay= @"Competitive salary";
         LCC.hours= @"9-5, Wednes-Sun";
-        LCC.Description=@"Maintaining the graveyard";
+        LCC.Description=@"Maintaining the graveyards in Headingley\nEmail:leedscitycouncil@hotmail.co.uk";
         LCC.Type=@"Grave digger";
         LCC.Image_2= [UIImage imageNamed: @"LCC.jpg"];//https://www.ncass.org.uk/images/default-source/default-album/leeds-city-council711BD99E9F14.jpg?sfvrsn=2
         
